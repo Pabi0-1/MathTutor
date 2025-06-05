d5b2776cda11e27096efd6dad29661bcc91a5c8f
Pabi0-1 <juanaguerra11969@gmail.com>
2025-06-05 14:01:30 -0700

    Initial Commit

---
+++ MathTutor/MathTutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathTutorApp.swift
+//  MathTutor
+//
+//  Created by Student1 on 6/5/25.
+//
+
+import SwiftUI
+
+@main
+struct MathTutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
